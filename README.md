1f2529431e6034f7d0151fea27f01bdc97137892
Pedro Garrett <pmcgt@iscte-iul.pt>
2025-11-21 11:14:10 +0000

    Add exercise 11

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ip25-string-exercises
 
 This includes the exercises for week 7 (strings).
+The exercises can be found inside src/exercises/setXX
+The common folder holds classes that are shared between different exercises
--- src/exercises/set02/Ex11.java
+++ src/exercises/set02/Ex11.java
@@ -1,72 +1,13 @@
 void main() {
-    startTicTacToe();
+    int[] array = {1, 2, 3};
+    IO.println(toText(array, " "));
+    IO.println(toText(array, ", "));
 }
 
-void startTicTacToe() {
-    int[][] gameboard = new int[3][3];
-    boolean isFirstPlayerTurn = true;
-
-    while (!isBoardFull(gameboard)) {
-        int playingNumber = isFirstPlayerTurn ? 1 : 2;
-        int[] symbolPosition = getPlayerChoice(gameboard, isFirstPlayerTurn);
-        gameboard[symbolPosition[0]][symbolPosition[1]] = playingNumber;
-        printBoard(gameboard);
-        isFirstPlayerTurn = !isFirstPlayerTurn;
+String toText(int[] array, String operator) {
+    String newStr = Integer.toString(array[0]);
+    for (int i = 1; i < array.length; i++) {
+        newStr += operator + array[i];
     }
+    return newStr;
 }
-
-int[] getPlayerChoice(int[][] gameboard, boolean isFirstPlayerTurn) {
-    String playingSymbol = isFirstPlayerTurn ? "X" : "O";
-    String rowResponse = IO.readln(playingSymbol + " linha: ");
-    String columnResponse = IO.readln(playingSymbol + " coluna: ");
-
-    int row = Integer.parseInt(rowResponse);
-    int column = Integer.parseInt(columnResponse);
-
-    while (!isPlayerChoiceValid(gameboard, row, column)) {
-        IO.println("That spot is already taken or invalid position, try again");
-        rowResponse = IO.readln(playingSymbol + " linha: ");
-        columnResponse = IO.readln(playingSymbol + " coluna: ");
-
-        row = Integer.parseInt(rowResponse);
-        column = Integer.parseInt(columnResponse);
-    }
-    return new int[]{row, column};
-}
-
-boolean isPlayerChoiceValid(int[][] gameboard, int row, int column) {
-    return row < gameboard.length && column < gameboard[0].length && gameboard[row][column] == 0;
-}
-
-
-void printBoard(int[][] gameboard) {
-    IO.println(Arrays.toString(gameboard[0]));
-    for (int i = 0; i < gameboard.length; i++) {
-        IO.println(i == 0 ? "" : "-----------");
-        for (int j = 0; j < gameboard[i].length; j++) {
-            String playingSymbol = "   ";
-            IO.print(j == 0 ? "" : "|");
-            if (gameboard[i][j] == 1) {
-                playingSymbol = " X ";
-            }
-            if (gameboard[i][j] == 2) {
-                playingSymbol = " O ";
-            }
-            IO.print(playingSymbol);
-        }
-        IO.println();
-    }
-    IO.println();
-}
-
-boolean isBoardFull(int[][] gameboard) {
-    for (int i = 0; i < gameboard.length; i++) {
-        for (int j = 0; j < gameboard[i].length; j++) {
-            if (gameboard[i][j] == 0) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
